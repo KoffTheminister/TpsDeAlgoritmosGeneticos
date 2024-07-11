0e469161d6543293a1fece5f577c46e236db2cb2
fabri <Fabririguetto@gmail.com>
2024-07-11 17:55:00 -0300

    parte 1 y 2

--- tpN2.py
+++ tpN2.py
@@ -2,62 +2,138 @@ tablaDeValores = [[150, 20], [325, 40], [600, 50], [805, 36], [430, 25], [1200, 
 numero_de_items = len(tablaDeValores)
 volMaximo = 4200
 listasPosibles = []
+listas = []
+elementos = []
 cont_tot = 0
+
 
 def precio_total(lista):
     tot = 0
     for i in range(len(lista)):
-        tot += tablaDeValores[lista[i],1]
+        if(lista[i] == '1'):
+            tot += tablaDeValores[i][1]
     return tot
 
 def vol_total(lista):
     tot = 0
     for i in range(len(lista)):
-        tot += tablaDeValores[lista[i],0]
+        if(lista[i] == '1'):
+            tot += tablaDeValores[i][0]
     return tot
 
-def agregar_o_no(lista):
-    i = 0
+def ordenamiento(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if precio_total(arr[j]) > precio_total(arr[j + 1]):
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+#Exaustiva
+def generar_exhaustiva():
+    for i in range(1025):
+        numfin=list()
+        binario_sin_prefijo = bin(i)[2:]
+        num = str(binario_sin_prefijo)
+        num = list(binario_sin_prefijo)
+        largo = 10 - len(num)
+        if (largo > 0):
+          for j in range(largo):
+            numfin.append('0')
+          for k in range(10-largo):
+            numfin.append(num[k])
+        else:
+            numfin= num
+        if(vol_total(num) <= volMaximo):
+            listasPosibles.append(numfin)
+        listas.append(numfin)
+
+def armar_listas_de_objetos(lista):
+    listas_de_objetos = []
+    totales = []
+
+    for elemento in lista:
+        objetos = []
+        total_volumen = 0
+        total_precio = 0
+        
+        for j in range(10):
+            if (elemento[j] == '1'):
+                objetos.append(tablaDeValores[j])
+                total_volumen += tablaDeValores[j][0]
+                total_precio += tablaDeValores[j][1]
+        
+        listas_de_objetos.append(objetos)
+        totales.append((total_volumen, total_precio))
+
+    return listas_de_objetos, totales
+
+
+
+#Greedy
+def valorProporcional():
+    valoresProporcionales = []
+    for i in range(len(tablaDeValores)):
+       valoresProporcionales.append([i, tablaDeValores[i][1]/tablaDeValores[i][0]])
+   
+    #ordenamiento
+    n = len(valoresProporcionales)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if (valoresProporcionales[j][1] > valoresProporcionales[j + 1][1]):
+                valoresProporcionales[j], valoresProporcionales[j + 1] = valoresProporcionales[j + 1], valoresProporcionales[j]
+            
     lock = True
-    while (lock and i < len(listasPosibles)):
-        lock2 = True
-        i2 = 0
-        while (lock2 == True and i2 < len(listasPosibles[i])):
-            if (repeticion_de_item(lista[i2],listasPosibles[i]) == False):
-                i2 += 1
-            else:
-                lock2 = False
-        if(lock2):
+    indice = 0
+    lista = ['0','0','0','0','0','0','0','0','0','0']
+    while (lock):
+        lista[valoresProporcionales[indice][0]] = '1'
+        if(vol_total(lista) <= volMaximo):
+            indice += 1
+        else:
+            lista[valoresProporcionales[indice][0]] = '0'
             lock = False
-        else:
-            i += 1
-    if(lock == True):
-        listasPosibles.append(lista)
 
-def repeticion_de_item(index, lista):
-    i = 0
-    while (i < len(lista)):
-        if(index == lista[i]):
-            return True
-        else:
-            i += 1
-    return False
+    return lista   
+
+def armar_listas_de_objeto_greedy(lista):
+    listas_de_objetos = []
+    totales = []
+    total_volumen = 0
+    total_precio = 0
+
+    for i in range(len(lista)):
+        objetos = []
+        if (lista[i] == '1'):
+            objetos.append(tablaDeValores[i])
+            total_volumen += tablaDeValores[i][0]
+            total_precio += tablaDeValores[i][1]
+        
+        listas_de_objetos.append(objetos)
+    totales.append((total_volumen, total_precio))
+
+    return listas_de_objetos, totales
 
 
-def funcion(passed_aux): #no se me ocurrio ningun otro nombre xd
-    my_aux = passed_aux.copy()
-    cont_de_inhabilidades = 0
-    for j in range(numero_de_items):
-        indice_repetido = repeticion_de_item(j, my_aux)
-        if(indice_repetido == False):
-            my_aux.append(j)
-            if(vol_total(my_aux) > volMaximo):
-                cont_de_inhabilidades += 1
-                my_aux.pop(len(my_aux) - 1)
-            else:
-                me_agrego = funcion(my_aux)
-                if(me_agrego == numero_de_items):
-                    agregar_o_no(my_aux)
-        else:
-            cont_de_inhabilidades += 1
-        return cont_de_inhabilidades
+
+#exhaustivo
+generar_exhaustiva()
+
+ordenamiento(listas)
+ordenamiento(listasPosibles)
+
+listas_de_objetos, totales = armar_listas_de_objetos(listas)
+
+print('exhaustiva')
+print('Todas las listas posibles:')
+for i, (lista_objetos, total) in enumerate(zip(listas_de_objetos, totales)):
+    print(f"Lista {i + 1}: {lista_objetos} - Volumen Total: {total[0]}, Precio Total: {total[1]}")
+
+#greedy
+listas_de_objetos, totales = armar_listas_de_objeto_greedy(valorProporcional())
+print('')
+print('')
+print('')
+print('')
+print('Greedy')
+print('lista: ', listas_de_objetos, ' - Volumen Total ', totales[0][0], ' - Valor Total ', totales[0][1])
+print()
