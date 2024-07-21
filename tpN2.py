fa8c774b825dfffa8de21c45a3d8518542801308
KoffTheMinister <nachokoffler@gmail.com>
2024-07-20 22:13:59 -0300

    :)

--- tpN2.py
+++ tpN2.py
@@ -2,62 +2,60 @@ tablaDeValores = [[150, 20], [325, 40], [600, 50], [805, 36], [430, 25], [1200, 
 numero_de_items = len(tablaDeValores)
 volMaximo = 4200
 listasPosibles = []
+listas = []
 cont_tot = 0
 
 def precio_total(lista):
     tot = 0
     for i in range(len(lista)):
-        tot += tablaDeValores[lista[i],1]
+        if(lista[i] == '1'):
+            tot += tablaDeValores[i][1]
     return tot
 
 def vol_total(lista):
     tot = 0
     for i in range(len(lista)):
-        tot += tablaDeValores[lista[i],0]
+        if(lista[i] == '1'):
+            tot += tablaDeValores[i][0]
     return tot
 
-def agregar_o_no(lista):
-    i = 0
-    lock = True
-    while (lock and i < len(listasPosibles)):
-        lock2 = True
-        i2 = 0
-        while (lock2 == True and i2 < len(listasPosibles[i])):
-            if (repeticion_de_item(lista[i2],listasPosibles[i]) == False):
-                i2 += 1
-            else:
-                lock2 = False
-        if(lock2):
-            lock = False
-        else:
-            i += 1
-    if(lock == True):
-        listasPosibles.append(lista)
+def ordenamiento(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if precio_total(arr[j]) > precio_total(arr[j + 1]):
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-def repeticion_de_item(index, lista):
-    i = 0
-    while (i < len(lista)):
-        if(index == lista[i]):
-            return True
-        else:
-            i += 1
-    return False
+def generar_exhaustiva():
+    for i in range(1025):
+        binario_sin_prefijo = bin(i)[2:]
+        num = str(binario_sin_prefijo)
+        num = list(binario_sin_prefijo)
+        largo = 10 - len(num)
+        if (largo > 0):
+          for j in range(largo):
+            num.append('0')
+        if(vol_total(num) <= volMaximo):
+            listasPosibles.append(num)
+        listas.append(num)
 
+def buscarElementos(lista):
+    Elementos = []
+    for i in range(len(lista)):
+        for j in range(10):
+            if lista[j] == '1':
+                Elementos.append(tablaDeValores[j,0],tablaDeValores[j,1])
 
-def funcion(passed_aux): #no se me ocurrio ningun otro nombre xd
-    my_aux = passed_aux.copy()
-    cont_de_inhabilidades = 0
-    for j in range(numero_de_items):
-        indice_repetido = repeticion_de_item(j, my_aux)
-        if(indice_repetido == False):
-            my_aux.append(j)
-            if(vol_total(my_aux) > volMaximo):
-                cont_de_inhabilidades += 1
-                my_aux.pop(len(my_aux) - 1)
-            else:
-                me_agrego = funcion(my_aux)
-                if(me_agrego == numero_de_items):
-                    agregar_o_no(my_aux)
-        else:
-            cont_de_inhabilidades += 1
-        return cont_de_inhabilidades
+                
+generar_exhaustiva()
+ordenamiento(listas)
+ordenamiento(listasPosibles)
+
+print('todas las listas:')
+print(listas)
+print('')
+print('')
+print('')
+print('')
+print('todas las listas posibles:')
+print(listasPosibles)
